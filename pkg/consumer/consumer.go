fca49d7a4588069a84098db9fd08b2e049de6c77
Meraiku <meraikuu@gmail.com>
2024-11-13 22:16:53 +0300

    Add single consumer subscribtion

--- pkg/consumer/consumer.go
+++ pkg/consumer/consumer.go
@@ -1,8 +1,14 @@
 package consumer
 
-import "github.com/IBM/sarama"
+import (
+	"context"
+	"log"
+	"math/rand/v2"
 
-func New(brokers []string) (*sarama.Consumer, error) {
+	"github.com/IBM/sarama"
+)
+
+func New(ctx context.Context, brokers []string, topic string) error {
 	config := sarama.NewConfig()
 
 	config.Consumer.Offsets.Initial = sarama.OffsetOldest
@@ -10,13 +16,17 @@ func New(brokers []string) (*sarama.Consumer, error) {
 
 	consumer, err := sarama.NewConsumer(brokers, config)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return &consumer, nil
+	if err := subscribe(ctx, topic, consumer); err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func NewGroup(brokers []string, groupID string) (*sarama.ConsumerGroup, error) {
+func NewGroup(brokers []string, groupID string) (sarama.ConsumerGroup, error) {
 	config := sarama.NewConfig()
 
 	config.Consumer.Offsets.Initial = sarama.OffsetOldest
@@ -27,5 +37,36 @@ func NewGroup(brokers []string, groupID string) (*sarama.ConsumerGroup, error) {
 		return nil, err
 	}
 
-	return &consumer, nil
+	return consumer, nil
+}
+
+func subscribe(ctx context.Context, topic string, consumer sarama.Consumer) error {
+
+	partitions, err := consumer.Partitions(topic)
+	if err != nil {
+		return err
+	}
+
+	pc, err := consumer.ConsumePartition(topic, partitions[rand.IntN(len(partitions))], sarama.OffsetOldest)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		defer pc.Close()
+
+		log.Printf("Subscribed to topic '%s'\n", topic)
+
+		for msg := range pc.Messages() {
+			if err := ctx.Err(); err != nil {
+				log.Printf("Consumer cancelled: %v", err)
+				return
+			}
+			log.Printf("Consumed message from topic '%s': Partition: %d, Offset: %d, Key: %s, Value: %s\n",
+				msg.Topic, msg.Partition, msg.Offset, string(msg.Key), string(msg.Value))
+		}
+
+	}()
+
+	return nil
 }
