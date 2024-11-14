7d610df0144dfd5512594e9e36a745f81f528983
Meraiku <meraikuu@gmail.com>
2024-11-14 17:06:07 +0300

    Add producer configuration

--- pkg/producer/producer.go
+++ pkg/producer/producer.go
@@ -7,8 +7,9 @@ import (
 func NewSync(brokers []string) (sarama.SyncProducer, error) {
 
 	config := sarama.NewConfig()
+
 	config.Producer.Partitioner = sarama.NewRandomPartitioner
-	config.Producer.RequiredAcks = sarama.WaitForAll
+
 	config.Producer.Return.Successes = true
 
 	producer, err := sarama.NewSyncProducer(brokers, config)
@@ -22,8 +23,17 @@ func NewSync(brokers []string) (sarama.SyncProducer, error) {
 func NewAsync(brokers []string) (sarama.AsyncProducer, error) {
 
 	config := sarama.NewConfig()
+
 	config.Producer.Partitioner = sarama.NewRandomPartitioner
+
+	config.Producer.Idempotent = true
 	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Net.MaxOpenRequests = 1
+
+	// Example for correct transaction implementation for Exactly Once
+	config.Producer.Transaction.ID = "prefix.group_id.topic.paritition"
+	config.Consumer.IsolationLevel = sarama.ReadCommitted
+
 	config.Producer.Return.Successes = true
 
 	producer, err := sarama.NewAsyncProducer(brokers, config)
